1ddd570f02f07baf92641528bf0aab24d5003feb
1127071993@qq.com <aini2009626>
2019-09-01 20:46:16 +0800

    26-26 项目部署

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,11 @@ let path = require('path')
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
+const BASE_URL = process.env.NODE_ENV === 'production'?'/':'/'
 
 module.exports = {
   lintOnSave: false,
+  baseUrl:BASE_URL,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
